4f2db41eed9ab4a0e26b126e24b357363265cba9
Luan Nguyen <luan.nguyen10692@gmail.com>
2022-08-31 23:41:08 +0700

    update text

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Dashboard = () => {
-  return <div>Welcome to Dashboard test123 test123</div>;
+  return <div>Welcome to Dashboard</div>;
 };
 
 export default Dashboard;
